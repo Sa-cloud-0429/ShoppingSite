c9a9f6250bbcbeb3e21b4a447d3a41925b60531f
Sakura <s.shioda@a-force.co.jp>
2025-05-30 17:39:00 +0900

    commit

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -5,16 +5,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import beans.Beans;
+
 public class UserDAO extends DAO {
 
-	public Beans login(int memberId,String password) throws Exception {
+	public Beans login(int memberId, String password) throws Exception {
 		Beans user = null;
 
 		Connection con = getConnection();
 
 		PreparedStatement st = con.prepareStatement(
 				"select * from users where password = ? AND member_id=?");
-		st.setString(1,password);
+		st.setString(1, password);
 		st.setInt(2, memberId);
 		ResultSet rs = st.executeQuery();
 
@@ -23,13 +24,13 @@ public class UserDAO extends DAO {
 			user.setpassword(rs.getString("password"));
 			user.setlast_name(rs.getString("last_name"));
 			user.setfirst_name(rs.getString("first_name"));
-			
+
 		}
 		rs.close();
 		st.close();
 		con.close();
-		
+
 		return user;
-		
+
 	}
 }
--- src/main/webapp/views/login-in.jsp
+++ src/main/webapp/views/login-in.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
-	
+
 
 <!DOCTYPE html>
 <html>
@@ -10,18 +10,21 @@
 </head>
 <body>
 
-	<h1>ログイン</h1>
+	<h1>LOGIN</h1>
 
 	<form action="login-servlet" method="post">
+		<%---login-servletにデータ送信-----%>
 		<p>
-			ID: <input type="text" name="ID">
+			ID:<br> <input type="text" name="ID">
 		</p>
 		<br>
 		<p>
-			パスワード： <input type="password" name="password">
+			PASSWORD:<br> <input type="password" name="password">
 		</p>
-		<br> <input type="submit" value="ログイン"><br> <input
-			type="submit" value="新規会員登録"><br>
+		<br> <input type="submit" value="ログイン"><br>
+	</form>
+	<form action="member-servlet" method="post">
+		<input type="submit" value="新規会員登録"><br>
 
 	</form>
 
